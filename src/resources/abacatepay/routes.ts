633e4f2cd87d9adb5858bb76488099fc2d357397
JoseGet <getuliojose96@gmail.com>
2026-05-15 15:03:03 -0400

    Ajuste na rota de pagamento pix com acabatepay

--- src/resources/abacatepay/routes.ts
+++ src/resources/abacatepay/routes.ts
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.get("/payment/done/pix", listarPagamentoPix)
 
-router.post("/payment/pix", criarPagamentoPix);
+router.post("/payment/pix", isAuth, criarPagamentoPix);
 
 export default router;
